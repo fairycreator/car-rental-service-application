d1f8690903a2cfa4edb9a1172bc27815e2776be6
fairycreator <122693095+fairycreator@users.noreply.github.com>
2023-12-08 13:13:50 +0100

    SearchForm created

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,95 @@
+import Select from "react-select";
+import { useDispatch } from "react-redux";
+import { pricePoints, sortedMakes } from "../../utils/searchPointers";
+import { fetchCars } from "../../redux/operations";
+import { setFilter } from "../../redux/filterSlice";
+import { carsList } from "../../redux/advertSlice";
+import {
+  Form,
+  SubForm,
+  Title,
+  StyledSelect,
+  EnterText,
+  LabelMilage,
+  BtnSearch,
+} from "./SearchForm.styled";
+
+export const SearchForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { make, mileageFrom, mileageTo, rentalPrice } = e.target.elements;
+
+    if (
+      !make.value &&
+      !mileageFrom.value &&
+      !mileageTo.value &&
+      !rentalPrice.value
+    ) {
+      alert("Select any filter");
+      return;
+    }
+
+    const mileageFromInNumber = Number(mileageFrom.value);
+    const mileageToInNumber = Number(mileageTo.value);
+
+    if (mileageFromInNumber < 0 || mileageToInNumber < 0) {
+      alert("Select valid mileage");
+      return;
+    }
+
+    if (mileageFromInNumber > mileageToInNumber && mileageToInNumber !== 0) {
+      alert("Select mileage to more than mileage from");
+      return;
+    }
+
+    const filters = {
+      make: make.value,
+      rentalPrice: Number(rentalPrice.value),
+      mileageFrom: mileageFromInNumber,
+      mileageTo: mileageToInNumber,
+    };
+    dispatch(carsList());
+    dispatch(fetchCars({ page: 1, limit: 32 }));
+    dispatch(setFilter(filters));
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <SubForm>
+        <label>
+          <Title> Car brand </Title>
+          <Select
+            name="make"
+            placeholder="Enter the text"
+            options={sortedMakes}
+          />
+        </label>
+        <label>
+          <Title>Price / 1 hour </Title>
+          <StyledSelect
+            name="rentalPrice"
+            placeholder="To $"
+            options={pricePoints.map((price) => ({
+              value: price,
+              label: price === 0 ? "All" : `$${price}`,
+            }))}
+          />
+        </label>
+      </SubForm>
+      <div>
+        <Title> Car mileage / km</Title>
+        <EnterText>
+          <label>
+            <LabelMilage name="mileageFrom" placeholder="From" />
+          </label>
+          <label>
+            <LabelMilage name="mileageTo" placeholder="To" />
+          </label>
+        </EnterText>
+      </div>
+      <BtnSearch type="submit">Search</BtnSearch>
+    </Form>
+  );
+};
---
+++ src/components/SearchForm/SearchForm.styled.js
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+import Select from "react-select";
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-bottom: 20px;
+  background: #ffffff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+export const SubForm = styled.div`
+  display: flex;
+  gap: 10px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const Title = styled.h2`
+  font-size: 0.9rem;
+  color: #333;
+  margin-bottom: 5px;
+`;
+
+export const StyledSelect = styled(Select)`
+  .Select__control {
+    border: 1px solid #ced4da;
+    border-radius: 0.25rem;
+    padding: 4px 8px;
+    font-size: 0.9rem;
+
+    &:hover {
+      border-color: #adb5bd;
+    }
+  }
+
+  .Select__menu {
+    font-size: 0.9rem;
+  }
+
+  .Select__single-value {
+    color: #495057;
+  }
+`;
+
+export const EnterText = styled.div`
+  display: flex;
+  gap: 10px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const LabelMilage = styled.input.attrs({
+  type: "number",
+})`
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+  font-size: 0.9rem;
+  color: #495057;
+
+  &::placeholder {
+    color: #6c757d;
+  }
+`;
+
+export const BtnSearch = styled.button`
+  background-color: #0d6efd;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  font-size: 0.9rem;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #0b5ed7;
+  }
+`;
+
+// <StyledSelect name="make" placeholder="Enter the text" options={sortedMakes} />
+// <LabelMilage name="mileageFrom" placeholder="From" />
+// <LabelMilage name="mileageTo" placeholder="To" />
