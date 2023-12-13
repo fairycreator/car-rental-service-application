59c1d941581b822e17c5f63aec1d22dc9b5a2564
NatalyaMaterinskaya <natulka-91@ukr.net>
2023-12-13 20:19:56 +0200

    Added :root and base styles

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,10 +2,34 @@ import { createGlobalStyle } from 'styled-components';
 import 'modern-normalize';
 
 export const GlobalStyle = createGlobalStyle`
-body {
+:root{
+--primary-color-white:#FFFFFF; 
+--primary-color-black-one:#050505;
+--primary-color-violet:#B6C3FF;
+--primary-color-black-two:#0F0F0F;
+--primary-color-lite-green:#45FFBC;
+--primary-color-grey:#B6B6B6;
+--primary-color-green-lite:#E3FFA8;
+
+--secondary-color-grey-one:#292928;
+--secondary-color-pink:#FFC4F7;
+--secondary-color-yellow:#FFF3B7;
+
+
+--main-font-size: 14px;
+--main-line-height:1.4;
+--main-letter-spacing:0.02em; 
+--transition-dur-and-func: 250ms cubic-bezier(0.4, 0, 0.2, 1);
 }
 
-code {
+body {
+  margin: 0;
+  font-family: 'Poppins', sans-serif;
+  background-color: var(--primary-color-black-one);
+  color: var(--primary-color-white); 
+  font-size: var(--main-font-size);
+  line-height: var(--main-line-height);
+  letter-spacing: var(--main-letter-spacing);
 }
 
 h1,
@@ -29,13 +53,18 @@ ol {
 
 img {
     display: block;
+    max-width: 100%;
+    height: auto;
 }
 
 a
  {
-    text-decoration: none;
+  color: currentColor;
+  text-decoration: none;
 }
 
 button {
+    cursor: pointer;
 }
 `;
+
