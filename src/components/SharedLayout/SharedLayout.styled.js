80b6835a4adc12dcd7e31e6c241f544f9967e51a
va7ul <vas7ul@gmail.com>
2023-12-14 00:40:31 +0200

    add common padding to SharedLayout

---
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 16px;
+
+  @media screen and (min-width: 834px) {
+    padding-left: 27px;
+    padding-right: 27px;
+    padding-top: 24px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-left: 34px;
+    padding-right: 34px;
+    padding-top: 20px;
+  }
+`;
