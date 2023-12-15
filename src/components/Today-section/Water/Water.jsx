77256ef47ae7ecb1237df35a075dc6bbc9a579ba
1evanter <rita.oleksenkoo@gmail.com>
2023-12-15 19:41:49 +0200

    basic Water styles

--- src/components/Today-section/Water/Water.jsx
+++ src/components/Today-section/Water/Water.jsx
@@ -1,3 +1,40 @@
+import sprite from '../../../assets/images/sprite.svg';
+import {
+  Title,
+  Wrapper,
+  ProgressBar,
+  TrashIcon,
+  AddIcon,
+  DetailsWrapper,
+  SecondTitle,
+  Text,
+  Amount,
+  Button,
+} from './Water.styled';
+
 export const Water = () => {
-  return <h3>Water</h3>;
+  return (
+    <>
+      <Title>Water</Title>
+      <Wrapper>
+        <TrashIcon>
+          <use href={`${sprite}#icon-arrow-down`}></use>
+        </TrashIcon>
+        <ProgressBar>%</ProgressBar>
+        <DetailsWrapper>
+          <SecondTitle>Water consumption</SecondTitle>
+          <Text>
+            <Amount>1500</Amount>ml <br />
+            left: 450 ml
+          </Text>
+          <Button type="button">
+            <AddIcon>
+              <use href={`${sprite}#icon-add-converted`}></use>
+            </AddIcon>
+            Add water intake
+          </Button>
+        </DetailsWrapper>
+      </Wrapper>
+    </>
+  );
 };
---
+++ src/components/Today-section/Water/Water.styled.jsx
@@ -0,0 +1,80 @@
+import styled from 'styled-components';
+
+export const Title = styled.h3`
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 1.33;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  position: relative;
+  width: 300px;
+  gap: 20px;
+  padding: 16px 12px;
+  margin-top: 6px;
+  border-radius: 12px;
+  background-color: var(--primary-color-black-two);
+`;
+
+export const ProgressBar = styled.div`
+  width: 80px;
+  height: 192px;
+  border-radius: 20px;
+  border: 1px solid var(--primary-color-grey);
+  background-color: white;
+`;
+
+export const DetailsWrapper = styled.div`
+  padding: 22px 0;
+`;
+
+export const SecondTitle = styled.h4`
+  margin-bottom: 12px;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 1.37;
+`;
+
+export const Text = styled.p`
+  margin-bottom: 16px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.42;
+  color: var(--primary-color-grey);
+`;
+
+export const Amount = styled.span`
+  margin-right: 8px;
+  font-size: 28px;
+  font-weight: 500;
+  line-height: 1.21;
+  color: var(--primary-color-white);
+`;
+
+export const TrashIcon = styled.svg`
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  width: 20px;
+  height: 20px;
+  fill: var(--primary-color-green-lite);
+`;
+
+export const AddIcon = styled.svg`
+  width: 16px;
+  height: 16px;
+  fill: var(--primary-color-black-two);
+`;
+
+export const Button = styled.button`
+  align-items: center;
+  padding: 8px 10px;
+  color: var(--primary-color-black-two);
+  background-color: var(--primary-color-green-lite);
+  border-color: transparent;
+  border-radius: 12px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.42;
+`;
