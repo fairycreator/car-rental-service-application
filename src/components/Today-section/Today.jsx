74a2a4b7a055fc3ab4d6427ee9063916607c575e
1evanter <rita.oleksenkoo@gmail.com>
2023-12-15 17:54:01 +0200

    update Today styles

--- src/components/Today-section/Today.jsx
+++ src/components/Today-section/Today.jsx
@@ -1,16 +1,39 @@
-import { Link } from 'react-router-dom';
 import { DailyGoal } from './DailyGoal/DailyGoal';
 import { Food } from './Food/Food';
 import { Water } from './Water/Water';
+import sprite from '../../assets/images/sprite.svg';
+import {
+  Wrapper,
+  TitleWrapper,
+  List,
+  Title,
+  StyledLink,
+  Icon,
+} from './Today.styled';
 
 export const Today = () => {
   return (
-    <>
-      <h2>Today</h2>
-      <Link to="/dashboard">On the way to the goal</Link>
-      <DailyGoal />
-      <Food />
-      <Water />
-    </>
+    <Wrapper>
+      <TitleWrapper>
+        <Title>Today</Title>
+        <StyledLink to="/dashboard">
+          On the way to the goal
+          <Icon>
+            <use href={`${sprite}#icon-arrowright`}></use>
+          </Icon>
+        </StyledLink>
+      </TitleWrapper>
+      <List>
+        <li>
+          <DailyGoal />
+        </li>
+        <li>
+          <Food />
+        </li>
+        <li>
+          <Water />
+        </li>
+      </List>
+    </Wrapper>
   );
 };
---
+++ src/components/Today-section/Today.styled.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const Title = styled.h2`
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 1.25;
+`;
+
+export const StyledLink = styled(Link)`
+  align-items: center;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.42;
+  color: var(--primary-color-grey);
+`;
+
+export const Icon = styled.svg`
+  margin-left: 6px;
+  width: 16px;
+  height: 16px;
+  fill: var(--primary-color-grey);
+`;
