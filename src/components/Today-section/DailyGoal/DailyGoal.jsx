60c7b216f41c79d5ea5369acc1a06f0a347e5709
1evanter <rita.oleksenkoo@gmail.com>
2023-12-15 17:00:16 +0200

    basic DailyGoal styles

--- src/components/Today-section/DailyGoal/DailyGoal.jsx
+++ src/components/Today-section/DailyGoal/DailyGoal.jsx
@@ -1,3 +1,35 @@
+import sprite from '../../../assets/images/sprite.svg';
+import {
+  Wrapper,
+  List,
+  BubbleIcon,
+  MilkIcon,
+  Text,
+  Amount,
+} from './DailyGoal.styled';
+
 export const DailyGoal = () => {
-  return <h3>Daily goal</h3>;
+  return (
+    <Wrapper>
+      <h3>Daily goal</h3>
+      <List>
+        <li>
+          <BubbleIcon>
+            <use href={`${sprite}#icon-bubble`}></use>
+          </BubbleIcon>
+          <Text>
+            Calories <Amount>1700</Amount>
+          </Text>
+        </li>
+        <li>
+          <MilkIcon>
+            <use href={`${sprite}#icon-milk`}></use>
+          </MilkIcon>
+          <Text>
+            Water <Amount>1500</Amount> ml
+          </Text>
+        </li>
+      </List>
+    </Wrapper>
+  );
 };
---
+++ src/components/Today-section/DailyGoal/DailyGoal.styled.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 300px;
+  height: 212px;
+`;
+
+export const List = styled.ul`
+  background-color: var(--primary-color-black-two);
+`;
+
+export const BubbleIcon = styled.svg`
+  width: 80px;
+  height: 80px;
+  fill: var(--primary-color-lite-green);
+`;
+
+export const MilkIcon = styled.svg`
+  width: 80px;
+  height: 80px;
+  fill: var(--primary-color-violet);
+`;
+
+export const Text = styled.p`
+  color: var(--primary-color-grey);
+`;
+
+export const Amount = styled.span`
+  color: var(--primary-color-white);
+`;
