6d473233ec3edcc8ee9ca2d188824cd144f8743c
VorontsovaYulia <vorontsovayulia89@gmail.com>
2023-12-16 02:31:49 +0200

    header-add_Goal_Nav

---
+++ src/components/GoalNav/GoalNav.styled.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const LoseFat = styled.img`
+  width: 28px;
+  height: 28px;
+`;
+
+export const DivStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 2px;
+`;
+
+export const DivImage = styled.div`
+  border: 1px solid #292928;
+  border-radius: 12px;
+  background: rgba(255, 255, 255, 0.01);
+  padding: 10px;
+`;
+
+export const MainText = styled.p`
+  font-family: 'Poppins600', sans-serif;
+  font-size: var(--main-font-size);
+  font-style: normal;
+  line-height: var(--main-line-height);
+  color: var(--primary-color-white);
+`;
+
+export const Text = styled.p`
+  font-family: 'Poppins500', sans-serif;
+  font-size: var(--main-font-size);
+  font-style: normal;
+  line-height: var(--main-line-height);
+  color: var(--primary-color-white);
+`;
