47d8a048a5f368eaef301a4f08571ea6fd91637f
1evanter <rita.oleksenkoo@gmail.com>
2023-12-17 19:21:49 +0200

    FoodCard component styles

---
+++ src/components/Today-section/Food/FoodCard/FoodCard.styled.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const ChartWrapper = styled.div`
+  width: 48px;
+  height: 48px;
+  border-radius: 100%;
+  background-color: white;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  gap: 12px;
+`;
+
+export const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+`;
+
+export const Goal = styled.div``;
