77d85177e52f15a2d8ceb6d6f54e4c2a66a22979
NatalyaMaterinskaya <natulka-91@ukr.net>
2023-12-17 16:12:12 +0200

    Added styles for weight table (mobile)

--- src/components/Dashboard/WeightTable/WeightTable.jsx
+++ src/components/Dashboard/WeightTable/WeightTable.jsx
@@ -1,12 +1,10 @@
-import {
-  CustomTable,
-  CustomTableBody,
-  WeightChartGlobalStyles,
-  CustomTableCell,
-  CustomTableContainer,
-  CustomTableRow,
-  CustomPaper,
-} from './WeightTable.styled';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+
+import { CustomTableRowUp, CustomTableRowDown } from './WeightTable.styled';
+
 function createData(day, weight) {
   return { day, weight };
 }
@@ -19,31 +17,48 @@ for (let i = 1; i < 31; i++) {
 export const WeightTable = () => {
   return (
     <>
-      <CustomTableContainer component={CustomPaper}>
-        <CustomTable sx={{ minWidth: 650 }} aria-label="simple table">
-          <CustomTableBody>
-            <CustomTableRow
+      <TableContainer
+        sx={{
+          backgroundColor: '#0F0F0F',
+          borderRadius: '12px',
+          paddingLeft: '10px',
+          paddingRight: '10px',
+          '&::-webkit-scrollbar': {
+            height: '8px',
+          },
+          '&::-webkit-scrollbar-track': {
+            '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: 'rgb(255, 255, 255, 0.1)',
+            outline: '1px solid rgb(255, 255, 255, 0.1)',
+            borderRadius: '12px',
+          },
+        }}
+      >
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableBody>
+            <CustomTableRowUp
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               {column.map((column) => (
-                <CustomTableCell key={column.day} component="th" scope="row">
+                <TableCell key={column.day} component="th" scope="row">
                   {column.day}
-                </CustomTableCell>
+                </TableCell>
               ))}
-            </CustomTableRow>
-            <CustomTableRow
+            </CustomTableRowUp>
+            <CustomTableRowDown
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               {column.map((column) => (
-                <CustomTableCell key={column.day} align="right">
+                <TableCell key={column.day} align="right">
                   {column.weight}
-                </CustomTableCell>
+                </TableCell>
               ))}
-            </CustomTableRow>
-          </CustomTableBody>
-        </CustomTable>
-      </CustomTableContainer>
-      <WeightChartGlobalStyles />
+            </CustomTableRowDown>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
--- src/components/Dashboard/WeightTable/WeightTable.styled.js
+++ src/components/Dashboard/WeightTable/WeightTable.styled.js
@@ -1,39 +1,30 @@
-import { styled, createGlobalStyle } from 'styled-components';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
+import { styled,} from 'styled-components';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
-export const CustomTable = styled(Table)`
-  background-color: var(--primary-color-black-two);
-`;
-
-export const CustomTableBody = styled(TableBody)`
- 
-`;
-
-export const CustomTableCell = styled(TableCell)({
-
-});
-
-export const CustomTableContainer = styled(TableContainer)`
-  color: var(--primary-color-white);
-`;
-
-export const CustomTableRow = styled(TableRow)({
+export const CustomTableRowUp = styled(TableRow)({
   '& .MuiTableCell-root': {
+    fontFamily: 'Poppins400, sans-serif',
+    fontSize: '14px',
+    lineHeight: '1.4',
     color: '#FFFFFF',
+    borderBottom: 'none',
+    paddingTop: '24px',
+    paddingLeft: '12px',
+    paddingRight: '12px',
+    paddingBottom: '8px',
   },
 });
-export const CustomPaper = styled(Paper)`
-
-`;
-
-
-
-
-export const WeightChartGlobalStyles = createGlobalStyle`
-
-`;
+export const CustomTableRowDown = styled(TableRow)({
+  '& .MuiTableCell-root': {
+    fontFamily: 'Poppins400, sans-serif',
+    fontSize: '10px',
+    lineHeight: '1.4',
+    color: '#B6B6B6',
+    borderBottom: 'none',
+    paddingTop: '8px',
+    paddingLeft: '15px',
+    paddingRight: '15px',
+    paddingBottom: '36px',
+    textAlign: 'center',
+  },
+});
