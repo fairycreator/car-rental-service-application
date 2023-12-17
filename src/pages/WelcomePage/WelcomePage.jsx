f5f2e780750d9448d996ed8d45cb0d901257760e
fairycreator <122693095+fairycreator@users.noreply.github.com>
2023-12-17 22:01:56 +0100

    debuged image on welcome page

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import FeatureItem from '../../components/FeatureItem/FeatureItem';
-import image from '../../assets/backgroundImages/welcomepage.jpg';
+import image from '../../assets/backgroundImages/welcomepage.png';
 
 import {
   Container,
