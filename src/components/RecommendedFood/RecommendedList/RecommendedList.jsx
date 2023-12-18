4d0594d070c92258226f52505d3c873e89153b12
PetroKasperskyi <kaspersky28061993@gmail.com>
2023-12-18 10:16:06 +0200

    commit 002

--- src/components/RecommendedFood/RecommendedList/RecommendedList.jsx
+++ src/components/RecommendedFood/RecommendedList/RecommendedList.jsx
@@ -1,14 +1,22 @@
-
+import sprite from 'assets/images/sprite.svg';
 import { RecommendedCard } from "../RecommendedCard/RecommendedCard"
-
+import { RecLink, Image, RecLinkWrapper } from "./RecommendedList.styled"
 export const RecommendedList = () => 
 {
     return (
+        <div>
         <ul>
             <RecommendedCard />
             <RecommendedCard />
             <RecommendedCard />
              <RecommendedCard/>
-        </ul>
+            </ul>
+           <RecLinkWrapper>
+            <RecLink to='/recommended-food'>See more
+              <Image> <use href={`${sprite}#icon-arrowright`}></use></Image> 
+            </RecLink>
+           </RecLinkWrapper>
+
+        </div>
     )
 }
--- src/components/RecommendedFood/RecommendedList/RecommendedList.styled.js
+++ src/components/RecommendedFood/RecommendedList/RecommendedList.styled.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
 
 export const RecPageContentWrapper = styled.div`
 padding: 16px 10px;
 
-@media screen and (min-width: 838px){
+@media screen and (min-width: 768px){
     padding: 24px 27px;
 }
 
-@media screen and (min-width: 1440px){
+@media screen and (min-width: 1200px){
 
     padding: 20px 34px;
 }
@@ -21,11 +23,11 @@ flex-direction:column;
 gap: 24px;
 align-items: center;
 
-@media screen and (min-width: 838px){
+@media screen and (min-width: 768px){
     
 }
 
-@media screen and (min-width: 1440px){
+@media screen and (min-width: 1200px){
   flex-direction: row;
   gap: 44px;  
 }
@@ -37,15 +39,33 @@ width : 300px;
 height: 312px;
 
 
-@media screen and (min-width: 838px) {
+@media screen and (min-width: 768px) {
    width : 380px;
 height: 396px;
   }
 
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: 1200px) {
 width : 536px;
 height: 560px;
   }
 
+
+`
+
+export const RecLink = styled(NavLink)`
+font-size: 15px;
+`
+
+export const Image = styled.svg`
+width: 16px;
+height: 16px;
+fill: white;
+align-items: center;
+`
+export const RecLinkWrapper = styled.div`
+display: flex ; 
+gap: 6px;
+align-items: center;
+padding: 3px;
 
 `
