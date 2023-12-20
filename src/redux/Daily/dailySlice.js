a13bf417d82f69c72e940a95b69557b6840d3861
1evanter <rita.oleksenkoo@gmail.com>
2023-12-20 22:00:01 +0200

    update dailySlice

---
+++ src/redux/Daily/dailySlice.js
@@ -0,0 +1,62 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { getCurrentStatistics, addWater, deleteWater } from './dailyOperations';
+
+const handlePending = (state) => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const handleGetCurrentFulfilled = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  state.dailyStatistics = action.payload;
+};
+
+const handleAddWaterFulfilled = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  state.consumedWater += action.payload;
+};
+
+const handleDeleteWaterFulfilled = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  state.consumedWater = action.payload;
+};
+
+const consumedFood = {
+  breakfast: [{ name: '', carbs: 0, protein: 0, fat: 0, calories: 0 }],
+  lunch: [{ name: '', carbs: 0, protein: 0, fat: 0, calories: 0 }],
+  dinner: [{ name: '', carbs: 0, protein: 0, fat: 0, calories: 0 }],
+  snack: [{ name: '', carbs: 0, protein: 0, fat: 0, calories: 0 }],
+};
+
+const initialState = {
+  consumedWater: 0,
+  consumedFood,
+  isLoading: false,
+  error: null,
+};
+
+const dailyStatisticsSlice = createSlice({
+  name: 'dailyStatistics',
+  initialState,
+
+  extraReducers: (builder) =>
+    builder
+      .addCase(getCurrentStatistics.pending, handlePending)
+      .addCase(getCurrentStatistics.fulfilled, handleGetCurrentFulfilled)
+      .addCase(getCurrentStatistics.rejected, handleRejected)
+      .addCase(addWater.pending, handlePending)
+      .addCase(addWater.fulfilled, handleAddWaterFulfilled)
+      .addCase(addWater.rejected, handleRejected)
+      .addCase(deleteWater.pending, handlePending)
+      .addCase(deleteWater.fulfilled, handleDeleteWaterFulfilled)
+      .addCase(deleteWater.rejected, handleRejected),
+});
+
+export const dailyStatisticsReducer = dailyStatisticsSlice.reducer;
