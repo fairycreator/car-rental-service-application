845e33a7077f6ec5624a89bd61cd2359e2281ec7
1evanter <rita.oleksenkoo@gmail.com>
2023-12-21 22:43:08 +0200

    fix add & delete operations

--- src/redux/dailyStatistics/dailyOperations.js
+++ src/redux/dailyStatistics/dailyOperations.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+// import { useSelector } from 'react-redux';
+// import { selectUserToken } from '../auth/authSelectors';
 
 export const instance = axios.create({
-  baseURL: 'https://healthy-life-backend-b6ck.onrender.com/api/',
+  baseURL: 'https://healthy-life-backend-b6ck.onrender.com/api',
 });
 
-// export const getCurrentStatistics = createAsyncThunk(
+// export const getDailyStatistics = createAsyncThunk(
 //   'dailyStatistics/getCurrent',
 //   async (_, thunkAPI) => {
 //     try {
-//       const response = await instance.get('/user/current');
+//       const response = await instance.get('/user/dailyStatistics');
 //       return response.data;
 //     } catch (error) {
 //       return thunkAPI.rejectWithValue(error.message);
@@ -21,6 +23,8 @@ export const addWater = createAsyncThunk(
   'dailyStatistics/addWater',
   async (water, thunkAPI) => {
     try {
+      const { token } = thunkAPI.getState().auth;
+      instance.defaults.headers['Authorization'] = `Bearer ${token}`;
       const response = await instance.post('/user/water-intake', water);
       return response.data;
     } catch (error) {
@@ -33,7 +37,11 @@ export const deleteWater = createAsyncThunk(
   'dailyStatistics/deleteWater',
   async (_id, thunkAPI) => {
     try {
-      const response = await instance.delete('/user/water-intake', _id);
+      const { token } = thunkAPI.getState().auth;
+      instance.defaults.headers['Authorization'] = `Bearer ${token}`;
+      const response = await instance.delete('/user/water-intake', {
+        data: { _id },
+      });
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
