3f0f4e47ba83cb746a35d046328cabcad3209f90
va7ul <vas7ul@gmail.com>
2023-12-22 16:11:39 +0200

    add recommended food list on main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,18 @@
+import { useDispatch } from 'react-redux';
+import { fetchRecFoods } from '../redux/recomendedFoods/recOperations';
+import { useEffect } from 'react';
 import { Today } from '../components/Today-section/Today';
 import { RecommendedList } from '../components/RecommendedFood/RecommendedList/RecommendedList';
+
 const MainPage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRecFoods());
+  }, [dispatch]);
   return (
     <>
       <Today />
-      <RecommendedList/>
+      <RecommendedList />
     </>
   );
 };
