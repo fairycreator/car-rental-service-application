6c7687464ab098b106bf8c7aa6e285ebd36b6fbd
va7ul <vas7ul@gmail.com>
2023-12-22 16:10:24 +0200

    fixed padding on common page

--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -2,17 +2,23 @@ import styled from 'styled-components';
 import { hidden } from '../../GlobalStyle';
 
 export const Container = styled.div`
+  max-width: 320px;
+  margin: 0 auto;
   padding-left: 10px;
   padding-right: 10px;
   padding-top: 16px;
 
   @media screen and (min-width: 834px) {
+    max-width: 834px;
+    margin: 0 auto;
     padding-left: 27px;
     padding-right: 27px;
     padding-top: 24px;
   }
 
   @media screen and (min-width: 1440px) {
+    max-width: 1440px;
+    margin: 0 auto;
     padding-left: 34px;
     padding-right: 34px;
     padding-top: 20px;
