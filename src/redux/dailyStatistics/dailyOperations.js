7ed24d7e4f32130ee5c30f47e7d0eff6fe0e5362
1evanter <rita.oleksenkoo@gmail.com>
2023-12-23 17:00:10 +0200

    Update dailyOperations.js
    rename operation & add condition

--- src/redux/dailyStatistics/dailyOperations.js
+++ src/redux/dailyStatistics/dailyOperations.js
@@ -1,31 +1,42 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-// import { useSelector } from 'react-redux';
-// import { selectUserToken } from '../auth/authSelectors';
 
 export const instance = axios.create({
   baseURL: 'https://healthy-life-backend-b6ck.onrender.com/api',
 });
 
-// export const getDailyStatistics = createAsyncThunk(
-//   'dailyStatistics/getCurrent',
-//   async (_, thunkAPI) => {
-//     try {
-//       const response = await instance.get('/user/dailyStatistics');
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
+export const getDailyWater = createAsyncThunk(
+  'dailyWaterStatistics/getDailyWater',
+  async (_, thunkAPI) => {
+    try {
+      const { token } = thunkAPI.getState().auth;
+      instance.defaults.headers['Authorization'] = `Bearer ${token}`;
+      const response = await instance.get('/user/water-intake');
+      console.log(response);
+      if (response.data.waterIntakeRecord === null) {
+        console.log('lskdjkdls');
+        return {
+          waterIntakeRecord: {
+            value: 0,
+            _id: '',
+          },
+        };
+      }
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 export const addWater = createAsyncThunk(
-  'dailyStatistics/addWater',
+  'dailyWaterStatistics/addWater',
   async (water, thunkAPI) => {
     try {
       const { token } = thunkAPI.getState().auth;
       instance.defaults.headers['Authorization'] = `Bearer ${token}`;
       const response = await instance.post('/user/water-intake', water);
+      console.log(response);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -34,7 +45,7 @@ export const addWater = createAsyncThunk(
 );
 
 export const deleteWater = createAsyncThunk(
-  'dailyStatistics/deleteWater',
+  'dailyWaterStatistics/deleteWater',
   async (_id, thunkAPI) => {
     try {
       const { token } = thunkAPI.getState().auth;
