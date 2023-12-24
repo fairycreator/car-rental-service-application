5c625e9b94c3e528d9f7c4a8ae76bbc92dea3ea1
va7ul <vas7ul@gmail.com>
2023-12-24 12:29:55 +0200

    Update profileUpdateSchema.js

--- src/schemas/profileUpdateSchema.js
+++ src/schemas/profileUpdateSchema.js
@@ -42,10 +42,10 @@ export const validationSchema = Yup.object().shape({
     .max(250, 'Incorrect data for calculation')
     .required('Height should be filled'),
   weight: Yup.number('Enter correct number')
-    .positive('Height should be positive')
-    .integer('Height should be integer')
+    .positive('Weight should be positive')
+    .integer('Weight should be integer')
     .min(40, 'Incorrect data for calculation')
     .max(200, 'Incorrect data for calculation')
-    .required('Height should be filled'),
+    .required('Weight should be filled'),
   activityLevel: Yup.number().oneOf([1.2, 1.375, 1.55, 1.725, 1.9]).required(),
 });
