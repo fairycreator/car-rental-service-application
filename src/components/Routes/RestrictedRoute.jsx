d475e7a4b539bc72fd0a316c30b9e70165d71216
va7ul <vas7ul@gmail.com>
2023-12-24 16:47:39 +0200

    change navigation

--- src/components/Routes/RestrictedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-import { selectIsLoggedIn } from '../../redux/auth/authSelectors';
-
-export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
-  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
-};
