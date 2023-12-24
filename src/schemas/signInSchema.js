9249b26d9bccc20d3cc8f531647f90dea57d0b04
va7ul <vas7ul@gmail.com>
2023-12-24 16:53:16 +0200

    bug fix

--- src/schemas/signInSchema.js
+++ src/schemas/signInSchema.js
@@ -2,9 +2,13 @@ import * as Yup from 'yup';
 
 export const signInSchema = Yup.object().shape({
   email: Yup.string()
+    .trim()
+    .strict()
     .email('Enter a valid Email*')
     .required('Email is required!'),
   password: Yup.string()
+    .trim()
+    .strict()
     .min(6, 'Enter a valid Password*')
     .required('Password is required!'),
 });
