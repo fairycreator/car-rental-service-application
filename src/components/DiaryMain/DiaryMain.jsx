59c14815d5e67825bd43ce2ec91b4120cd679ca2
1evanter <rita.oleksenkoo@gmail.com>
2023-12-25 13:05:22 +0200

    add DiaryMainCard to DiaryMain

--- src/components/DiaryMain/DiaryMain.jsx
+++ src/components/DiaryMain/DiaryMain.jsx
@@ -1,30 +1,24 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+
 import {
   selectBreakfast,
   selectDinner,
   selectLunch,
   selectSnack,
 } from '../../redux/dailyFoodStatistics/foodSelectors';
-import { MealImage } from '../Diary-planner/Planner.styled';
-import { AddMore } from '../ReacordMealPopUp/Reacord.styled';
 import {
   ContainerDiary,
-  ContentBlock,
-  FoodBlock,
   FoodBlocks,
-  MealOptions,
-  MealTitle,
-  SeeMore,
+  StyledLink,
   Title,
-  TitleBlock,
 } from './DiaryMain.style';
+import { DiaryMainCard } from './DiaryMainCard/DiaryMainCard';
 
 const DiaryMain = () => {
   const breakfast = useSelector(selectBreakfast);
   const lunch = useSelector(selectLunch);
-  const snack = useSelector(selectSnack);
   const dinner = useSelector(selectDinner);
-
+  const snack = useSelector(selectSnack);
 
   const totalFatBreakfast = breakfast.reduce(
     (sum, item) => sum + item.nutrition?.fat,
@@ -60,227 +54,58 @@ const DiaryMain = () => {
     0
   );
 
-  const totalcarbogidrateBreakfast = breakfast.reduce(
+  const totalCarbsBreakfast = breakfast.reduce(
     (sum, item) => sum + item.nutrition?.carbogidrate,
     0
   );
-  const totalcarbogidrateDinner = dinner.reduce(
+  const totalCarbsDinner = dinner.reduce(
     (sum, item) => sum + item.nutrition?.carbogidrate,
     0
   );
-  const totalcarbogidrateLunch = lunch.reduce(
+  const totalCarbsLunch = lunch.reduce(
     (sum, item) => sum + item.nutrition?.carbogidrate,
     0
   );
-  const totalcarbogidrateSnack = snack.reduce(
+  const totalCarbsSnack = snack.reduce(
     (sum, item) => sum + item.nutrition?.carbogidrate,
     0
   );
 
-  const iconPath = 'src/assets/images/sprite.svg';
   return (
     <ContainerDiary>
       <div style={{ display: 'flex', gap: '16px', marginBottom: '16px' }}>
         <Title>Diary</Title>
-        <SeeMore>See more</SeeMore>
+        <StyledLink to="/diary">See more</StyledLink>
       </div>
       <FoodBlocks>
-        <ContentBlock>
-          <TitleBlock>
-            <MealImage src="src/assets/images/Breakfast.png" />
-            <MealTitle>BreakFast</MealTitle>
-          </TitleBlock>
-          <FoodBlock>
-            {breakfast[0].name !== '' ? (
-              <>
-                <MealOptions>
-                  Carbonohidrates: {totalcarbogidrateBreakfast}
-                </MealOptions>
-                <MealOptions>Protein: {totalProteinBreakfast}</MealOptions>
-                <MealOptions>
-                  Fat: {totalFatBreakfast}{' '}
-                  <svg
-                    style={{
-                      display: 'inline-block',
-                      width: '20px',
-                      height: ' 20px',
-                      fill: 'white',
-                    }}
-                  >
-                    <use href={`${iconPath}#trash-delete`}></use>
-                  </svg>
-                </MealOptions>
-              </>
-            ) : (
-              <div
-                style={{
-                  flexDirection: 'row-reverse',
-                  display: 'flex',
-                  gap: '8px',
-                }}
-              >
-                <AddMore>Record your meal</AddMore>
-                <svg
-                  style={{
-                    display: 'inline-block',
-                    width: '20px',
-                    height: ' 20px',
-                    fill: 'white',
-                  }}
-                >
-                  <use href={`${iconPath}#icon-add-converted`}></use>
-                </svg>
-              </div>
-            )}
-          </FoodBlock>
-        </ContentBlock>
-        <ContentBlock>
-          <TitleBlock>
-            <MealImage src="src/assets/images/Lunch.png" />
-            <MealTitle>Lunch</MealTitle>
-          </TitleBlock>
-          <FoodBlock>
-            {lunch[0].name !== '' ? (
-              <>
-                <MealOptions>
-                  Carbonohidrates: {totalcarbogidrateLunch}
-                </MealOptions>
-                <MealOptions>Protein: {totalProteinLunch}</MealOptions>
-                <MealOptions>
-                  Fat: {totalFatLunch}
-                  <svg
-                    style={{
-                      display: 'inline-block',
-                      width: '20px',
-                      height: ' 20px',
-                      fill: 'white',
-                    }}
-                  >
-                    <use href={`${iconPath}#trash-delete`}></use>
-                  </svg>
-                </MealOptions>
-              </>
-            ) : (
-              <div
-                style={{
-                  flexDirection: 'row-reverse',
-                  display: 'flex',
-                  gap: '8px',
-                }}
-              >
-                <AddMore>Record your meal</AddMore>
-                <svg
-                  style={{
-                    display: 'inline-block',
-                    width: '20px',
-                    height: ' 20px',
-                    fill: 'white',
-                  }}
-                >
-                  <use href={`${iconPath}#icon-add-converted`}></use>
-                </svg>
-              </div>
-            )}
-          </FoodBlock>
-        </ContentBlock>
-        <ContentBlock>
-          <TitleBlock>
-            <MealImage src="src/assets/images/Dinner.png" />
-            <MealTitle>Dinner</MealTitle>
-          </TitleBlock>
-          <FoodBlock>
-            {dinner[0].name !== '' ? (
-              <>
-                <MealOptions>
-                  Carbonohidrates: {totalcarbogidrateDinner}
-                </MealOptions>
-                <MealOptions>Protein: {totalProteinDinner}</MealOptions>
-                <MealOptions>
-                  Fat: {totalFatDinner}
-                  <svg
-                    style={{
-                      display: 'inline-block',
-                      width: '20px',
-                      height: ' 20px',
-                      fill: 'white',
-                    }}
-                  >
-                    <use href={`${iconPath}#trash-delete`}></use>
-                  </svg>
-                </MealOptions>
-              </>
-            ) : (
-              <div
-                style={{
-                  flexDirection: 'row-reverse',
-                  display: 'flex',
-                  gap: '8px',
-                }}
-              >
-                <AddMore>Record your meal</AddMore>
-                <svg
-                  style={{
-                    display: 'inline-block',
-                    width: '20px',
-                    height: ' 20px',
-                    fill: 'white',
-                  }}
-                >
-                  <use href={`${iconPath}#icon-add-converted`}></use>
-                </svg>
-              </div>
-            )}
-          </FoodBlock>
-        </ContentBlock>
-        <ContentBlock>
-          <TitleBlock>
-            <MealImage src="src/assets/images/Snack.png" />
-            <MealTitle>Snack</MealTitle>
-          </TitleBlock>
-          <FoodBlock>
-            {snack[0].name !== '' ? (
-              <>
-                <MealOptions>
-                  Carbonohidrates: {totalcarbogidrateSnack}
-                </MealOptions>
-                <MealOptions>Protein: {totalProteinSnack}</MealOptions>
-                <MealOptions>
-                  Fat: {totalFatSnack}{' '}
-                  <svg
-                    style={{
-                      display: 'inline-block',
-                      width: '20px',
-                      height: ' 20px',
-                      fill: 'white',
-                    }}
-                  >
-                    <use href={`${iconPath}#trash-delete`}></use>
-                  </svg>
-                </MealOptions>
-              </>
-            ) : (
-              <div
-                style={{
-                  flexDirection: 'row-reverse',
-                  display: 'flex',
-                  gap: '8px',
-                }}
-              >
-                <AddMore>Record your meal</AddMore>
-                <svg
-                  style={{
-                    display: 'inline-block',
-                    width: '20px',
-                    height: ' 20px',
-                    fill: 'white',
-                  }}
-                >
-                  <use href={`${iconPath}#icon-add-converted`}></use>
-                </svg>
-              </div>
-            )}
-          </FoodBlock>
-        </ContentBlock>
+        <DiaryMainCard
+          meal={breakfast}
+          title="Breakfast"
+          totalCarbs={totalCarbsBreakfast}
+          totalProtein={totalProteinBreakfast}
+          totalFat={totalFatBreakfast}
+        />
+        <DiaryMainCard
+          meal={lunch}
+          title="Lunch"
+          totalCarbs={totalCarbsLunch}
+          totalProtein={totalProteinLunch}
+          totalFat={totalFatLunch}
+        />
+        <DiaryMainCard
+          meal={dinner}
+          title="Dinner"
+          totalCarbs={totalCarbsDinner}
+          totalProtein={totalProteinDinner}
+          totalFat={totalFatDinner}
+        />
+        <DiaryMainCard
+          meal={snack}
+          title="Snack"
+          totalCarbs={totalCarbsSnack}
+          totalProtein={totalProteinSnack}
+          totalFat={totalFatSnack}
+        />
       </FoodBlocks>
     </ContainerDiary>
   );
